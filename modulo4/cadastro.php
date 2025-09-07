ae15b09fede46d9ce04aa8f512d7437d1e656a74
Gylmar Souza De Brito <gilmarsousa717@gmail.com>
2025-09-07 14:43:20 -0300

    up

--- modulo4/cadastro.php
+++ modulo4/cadastro.php
@@ -1,2 +1,17 @@
-use php para usurfruir do sistema de cadastro
-r
+
+<?php
+    include 'conexao.php';
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
