8218b05c92397c0d840e4855e0f293f567d91829
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-10-08 17:51:37 -0300

    up js

---
+++ Java/soma.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class soma {
+    public static void main(String args[]) {
+        int valor1 = 5;
+        int valor2 = 10;
+
+        if (valor1 > valor2) {
+            System.out.println("Valor 1 é maior que Valor 2");
+        }
+        System.out.println("soma dos valores: " + (valor1 + valor2));
+
+    }
+}
