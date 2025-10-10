77b665640c8373a9341200f95dfdc5a9f77d4d5d
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-10-10 15:11:57 -0300

    jv

---
+++ Java/caso_03.java
@@ -0,0 +1,21 @@
+public void caso_03() {
+    System.out.println("digite um dos comentados abaixo no menu");
+    System.out.println("1: comprar; 2: vender; 3: Listar; 4: sair");
+
+    Scanner scan = new Scanner(System.in);
+    int opcao = scan.nextInt();
+
+    switch(opcao) {
+        case 1:
+            System.out.println("Você escolheu comprar");
+            break;
+        case 2:
+            System.out.println("Você escolheu vender");
+            break;
+        case 3: 
+            System.out.println("Você escolheu listar");
+            break;
+        default:
+            System.out.println("Você escolheu sair");
+    }
+}
