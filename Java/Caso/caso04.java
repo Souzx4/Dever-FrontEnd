95c2b044037af5ebecb807740f2a60b4a04a0d18
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-10-15 23:58:32 -0300

    up java

--- Java/Caso/caso04.java
+++
@@ -1,9 +0,0 @@
-public class caso04 {
-    public boolean testeIdade(int idade) {
-        if(idade >= 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
