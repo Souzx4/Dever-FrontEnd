b10936878c98b1e79822c3c5a47c2d45cb0ff09f
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-10-15 23:31:48 -0300

    up

---
+++ Java/ProjetoMouse/src/main/java/com/mycompany/projetomouse/ProjetoMouse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetomouse;
+
+/**
+ *
+ * @author gilma
+ */
+public class ProjetoMouse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
