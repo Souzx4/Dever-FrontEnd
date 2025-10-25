242716a1284d3d023af6359df726d6f4cfba2704
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-10-25 14:42:56 -0300

    up js

---
+++ Js/condicoes.js
@@ -0,0 +1,15 @@
+var vel = 61
+console.log(`Sua velocidade é de ${vel}Km/h e estamos passando por um medidor de velocidade`)
+if(vel > 60) {
+    console.log('Você altapassou o limite da via. MULTADO')
+} else {
+    console.log('siga em frente')
+}
+
+var pais = 'frança'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
