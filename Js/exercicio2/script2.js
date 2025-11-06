9c548be1a96e29028b95a1a5e2c74c71901060ee
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-06 17:13:06 -0300

    up dever

--- Js/exercicio2/script2.js
+++ Js/exercicio2/script2.js
@@ -1,1 +1,20 @@
-alert("Ola")
+function Carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia
+        img.src = '../img/manha.png'
+        document.body.style.background = '#62a6b5ff'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+        img.src = '../img/tarde.png'
+        document.body.style.background = '#9c9f68ff'
+    } else {
+        //Boa noite
+        img.src = '../img/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
