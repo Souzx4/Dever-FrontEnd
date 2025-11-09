f8e3de671dc0d5a671afee16fd8e5f9f85c49dc9
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-09 15:30:44 -0300

    up

--- Js/exercicio2/script2.js
+++ Js/exercicio2/script2.js
@@ -1,6 +1,6 @@
 function Carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
