57f714a07dea15431dce8ce01094bb5a61d042e1
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-10 16:11:22 -0300

    up

---
+++ Js/exercicio4/script.js
@@ -0,0 +1,8 @@
+function Contador() {
+    var inicio = document.getEletelmentById('cont-i')
+    var fim = document.getElementById('cont-f')
+    var passo = document.getElementById('cont-p')
+    var res = document.getElementById('res')
+            
+
+}
