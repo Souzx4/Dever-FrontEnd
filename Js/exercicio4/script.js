de98d279543afd4dcde3860910b334515cdb2492
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-10 17:57:10 -0300

    up

--- Js/exercicio4/script.js
+++ Js/exercicio4/script.js
@@ -1,8 +1,21 @@
-function Contador() {
-    var inicio = document.getEletelmentById('cont-i')
-    var fim = document.getElementById('cont-f')
-    var passo = document.getElementById('cont-p')
+function contar() {
+    var inicio = document.getElementById('conti')
+    var fim = document.getElementById('contf')
+    var passo = document.getElementById('contp')
     var res = document.getElementById('res')
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (let c = i; c <= f; c += p) {
+            //contagem crescente
+        }
+    }
             
 
 }
