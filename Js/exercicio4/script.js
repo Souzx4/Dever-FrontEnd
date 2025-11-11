ba01cfb940727090c89a820134bf714daf849eab
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-10 22:10:16 -0300

    up

--- Js/exercicio4/script.js
+++ Js/exercicio4/script.js
@@ -1,21 +1,34 @@
 function contar() {
-    var inicio = document.getElementById('conti')
-    var fim = document.getElementById('contf')
-    var passo = document.getElementById('contp')
-    var res = document.getElementById('res')
+    let inicio = document.getElementById('conti')
+    let fim = document.getElementById('contf')
+    let passo = document.getElementById('contp')
+    let res = document.getElementById('res')
     
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar'
         alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        if (let c = i; c <= f; c += p) {
-            //contagem crescente
+        if (p <= 0) {
+            alert('Passo invalido! Considerando passo 1')
+            p = 1
         }
-    }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+            //contagem crescente
+                res.innerHTML += `${c} \u{1F449}`
+            }
             
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                // contagem decrecente
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }           
 
 }
