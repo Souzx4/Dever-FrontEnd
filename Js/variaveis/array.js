48c3cb11ce437689e077960ce14e81d6a5173026
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-12 11:19:14 -0300

    up

--- Js/variaveis/array.js
+++ Js/variaveis/array.js
@@ -1,3 +1,6 @@
-let num = []
-
-console.log(`Nosso vetor e o ${num}`)
+let num = [5, 8, 2, 9, 3]
+num[1] = 1
+num.push(7)
+num.sort()
+console.log(`Nosso vetor e o ${num} posições`)
+console.log(num.length)
