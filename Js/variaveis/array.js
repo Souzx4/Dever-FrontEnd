528333b25e73d276feebf2f27a722cfd44579643
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-12 12:12:43 -0300

    up array

--- Js/variaveis/array.js
+++ Js/variaveis/array.js
@@ -1,6 +1,13 @@
-let num = [5, 8, 2, 9, 3]
-num[1] = 1
-num.push(7)
+let num = [5, 8, 2, 7, 3]
+num[6] = 4
+num.push(9)
 num.sort()
 console.log(`Nosso vetor e o ${num} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(num.length)
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('valor não encontrado')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
