7fe67953287a2b67d7566d3c05d9682be0b65b55
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-11-19 11:53:12 -0300

    up

--- modulo3/minhasFinancas.js
+++ modulo3/minhasFinancas.js
@@ -1,7 +1,15 @@
 // 1. Espera a página carregar
 window.addEventListener('DOMContentLoaded', () => {
+    //tenta carregar dados salvos na memoria do navegador
+    carregarDadosSalvos()
     // 2. Roda a função principal para calcular tudo
     atualizarTodosOsTotais()
+
+    // O "Ouvinte": Fica vigiando se você digita algo na tabela para recalcular e salvar
+    document.querySelector('table').addEventListener('input', () => {
+        atualizarTodosOsTotais()
+        salvarDados() // salva os dados na memoria do navegador
+    })
 });
 
 function atualizarTodosOsTotais() {
@@ -12,14 +20,18 @@ function atualizarTodosOsTotais() {
 
     //FORMATA OS VALORES PARA MOEDA BRASILEIRA
     const formatoMoeda = { style: 'currency', currency: 'BRL'}
-    const formatoValor = totalValor.toLocaleString('pt-br', formatoMoeda)
-    const formatopago = totalPago.toLocaleString('pt-br', formatoMoeda)
+    /*const formatoValor = totalValor.toLocaleString('pt-br', formatoMoeda)     <- usar somente se quiser mostrar o total a receber
+    const formatopago = totalPago.toLocaleString('pt-br', formatoMoeda)*/       // <- usar somente se quiser mostrar o total já pago
     const formatoRestante = saldoRestante.toLocaleString('pt-br', formatoMoeda)
 
     // Atualiza o HTML no rodapé (tfoot) com os valores calculados
-    document.getElementById('res').innerHTML = `<strong>${formatoValor}</strong>`
-    document.getElementById('res2').innerHTML = `<strong>${formatopago}</strong>`
-    document.getElementById('res3').innerHTML = `<strong>${formatoRestante}</strong>`
+    /*document.getElementById('res').innerHTML = `<strong>${formatoValor}</strong>`      <- usar somente se quiser mostrar o total a receber
+    document.getElementById('res2').innerHTML = `<strong>${formatopago}</strong>`        <- usar somente se quiser mostrar o total já pago
+    document.getElementById('res3').innerHTML = `<strong>${formatoRestante}</strong>` */
+    const elementoRes = document.getElementById('res3')
+    if (elementoRes) {
+        elementoRes.innerHTML = `<strong>${formatoRestante}</strong>`
+    }
 }
 
 function calcularSoma(seletorClasse) {
@@ -34,8 +46,11 @@ function calcularSoma(seletorClasse) {
             texto = texto.split('=')[1] // pega " 270"
         }
 
-        // Remove espaços, troca virgula por ponto
-        const textoLimpo = texto.trim().replace(',', '.')
+        // Limpa o texto: tira R$, tira espaços, troca vírgula por ponto
+        const textoLimpo = texto.trim()
+            .replace('R$', '')
+            .replace(/\./g, '')
+            .replace(',', '.')
         
         // Converte para número e soma
         const valor = parseFloat(textoLimpo)
@@ -44,4 +59,51 @@ function calcularSoma(seletorClasse) {
         }
     })
     return soma
+}
+
+function adicionarLinha() {
+    // adicionar um corpo da tabela
+    const tbody = document.querySelector('tbody')
+    // cria um elemento na linha tr
+    const novaLinha = document.createElement('tr')
+    // adicionando os dados na linha
+    novaLinha.innerHTML = `
+        <td contenteditable="true">Nome do Cliente</td>
+        <td class="num" contenteditable="true">data</td>
+        <td class="num valor" contenteditable="true">0</td>
+        <td contenteditable="true">0</td>
+        <td class="num" contenteditable="true">0</td>
+        <td class="pagas" contenteditable="true">0</td>
+    `
+    tbody.appendChild(novaLinha)
+
+    // Atuaizar e salvar assim que cria a linha
+    atualizarTodosOsTotais()
+    salvarDados()
+}
+
+// --- FUNÇÕES DE BANCO DE DADOS (LOCALSTORAGE) ---
+
+function salvarDados() {
+    // Pega todo o conteúdo HTML de dentro do corpo da tabela
+    const conteudotabela = document.querySelector('tbody').innerHTML
+    // Salva no LocalStorage do navegador
+    localStorage.setItem('minhasFinancas', conteudotabela)
+}
+
+function carregarDadosSalvos() {
+    // verificar se existe algo salvo
+    const dados = localStorage.getItem('minhasFinancas')
+    if (dados) {
+        // se existir, substitui o corpo da tabela pelo que foi salvo
+        document.querySelector('tbody').innerHTML = dados
+    }
+}
+
+function resetarDados() {
+    // pergunta para confirmar se quer realmente resetar (segurança)
+    if ( confirm('Tem certeza que deseja resetar todos os dados?')) {
+        localStorage.removeItem('minhasFinancas') // apaga os dados salvos
+        location.reload() // recarrega a página
+    }
 }
